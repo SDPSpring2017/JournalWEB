ff760990bbd971a361d1665d6b95014c93d2881d
kkeely <kathleenkeelyuni@gmail.com>
2017-09-16 11:13:01 +1000

    sign up adds record to db, current user method (will need to move to global config later)

--- GlobalService.js
+++ GlobalService.js
@@ -1,11 +1,17 @@
-function ConnecttoDatabase()
-{
-  
-    
-    var bigOne = document.getElementById('testHeader');
-    var dbRef = firebase.database().ref().child('Test');
-    //sync changes
-    dbRef.on('value', snap => bigOne.innerText = snap.val());
-}
+
+(function() {
+    // Needs to move into GlobalService.js cause all pages will need a connection to database so to avoid repetative code :) 
+    // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCXzTuaN3VoNiTIigpfOV-Jzneg94Y6Bko",
+    authDomain: "typodatabase.firebaseapp.com",
+    databaseURL: "https://typodatabase.firebaseio.com",
+    projectId: "typodatabase",
+    storageBucket: "typodatabase.appspot.com",
+    messagingSenderId: "836867063447"
+  };
+  firebase.initializeApp(config);  
+   
+}());
 
 
--- SignInService.js
+++ SignInService.js
@@ -1,5 +1,7 @@
 // I know we should avoid global variables I just don't feel like figuring out how to do that :) 
-var txtEmail, txtPassword, btnSignIn, btnSignOut, btnSignUp;
+var txtEmail, txtPassword, btnSignIn, btnSignOut, btnSignUp,
+    dbRef = new Firebase("https://typodatabase.firebaseio.com"),
+    authClient;
 (function() {
     // Needs to move into GlobalService.js cause all pages will need a connection to database so to avoid repetative code :) 
     // Initialize Firebase
@@ -11,19 +13,18 @@ var txtEmail, txtPassword, btnSignIn, btnSignOut, btnSignUp;
     storageBucket: "typodatabase.appspot.com",
     messagingSenderId: "836867063447"
   };
-  firebase.initializeApp(config);  
-    
+  firebase.initializeApp(config); 
     getScreenElements();
     signInEvent();
-    signUpEvent();
-    
-    
+    authenticationMonitoring();
    
 }());
+
 
 function getScreenElements()
 {
     // Getting screen elements
+    // will break out into separate get statements to avoid global variables up top
     txtEmail = document.getElementById('txtEmail');
     txtPassword = document.getElementById('txtPassword');
     btnSignIn = document.getElementById('btnSignIn');
@@ -35,24 +36,53 @@ function signInEvent()
 {
     btnSignIn.addEventListener('click', e=>
                               {
-        const email = txtEmail.value;
-        const password = txtPassword.value;
-        const promise = getFirebaseAuth().signInWithEmailAndPassword(email, password);
-        promise.catch(e => console.log(e.message));
+ authClient = new FirebaseSimpleLogin(dbRef, function(error, user) {
+  if (error) {
+    console.log(error);
+  } else if (user) {
+    // user authenticated with Firebase
+    console.log("User ID: " + user.uid + ", Provider: " + user.provider);
+      authClient.login("password");
+  } else {
+    // user is logged out
+      alert("No users are currently logged in");
+  }
+});
     });
+}
+function authenticationMonitoring()
+{
+    var authRef = new Firebase("https://typodatabase.firebaseio.com/.info/authenticated");
+authRef.on("value", function(snap) {
+  if (snap.val() === true) {
+    alert("authenticated");
+  } else {
+    alert("not authenticated");
+  }
+});
 }
 
 // do we want to move sign up into a separate service?
 function signUpEvent()
 {
- btnSignUp.addEventListener('click', e=>
-                           {
-     // gotta make sure to check is valid email format
-     const email = txtEmail.value;
-        const password = txtPassword.value;
-        const promise = getFirebaseAuth().createUserWithEmailAndPassword(email, password);
-        promise.catch(e => console.log(e.message));
- });
+btnSignUp.addEventListener('click', e => {
+    var email = txtEmail.val();
+    var password = txtPassword.val();
+    authClient.createUser(email, password, function(err, user){
+        if(!err)
+            {
+               dbRef.child("user").child(user.uid).set({
+         FName: "I will",
+         LName: "fix this later but I'm tired yall",
+         Email: email,
+         Password: password
+            }
+        else
+            {
+                alert(error);                                       
+            }
+    })
+})
 }
 
 function getFirebaseAuth()
@@ -74,4 +104,12 @@ function realTimeAuthListenter()
             }
         
     });
+}
+
+function SingOut()
+{
+    btnSignOut.addEventListener('click', e=>
+                               {
+        authClient.logout();
+    })
 }
