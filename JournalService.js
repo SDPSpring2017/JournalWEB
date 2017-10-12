34996995f3f49ea6aa7ad6c7a1488c7a02241378
Max Sekula <sekula_m@hotmail.com>
2017-10-12 12:45:30 +1100

    Hides create entry button when entry is created

--- JournalService.js
+++ JournalService.js
@@ -1,7 +1,6 @@
 var txtTitle, numId, txtBody, btnSave, database, journals;
 
-function setUp()
-{
+function setUp() {
 	setUpDatabase();
 	firebase.auth().onAuthStateChanged(function(currentUser) {
 	if (currentUser) {
@@ -11,15 +10,13 @@ function setUp()
 });
 }
 
-function setUpDatabase()
-{
+function setUpDatabase() {
 	firebase.initializeApp(getConfig());
 	database = firebase.database();
 }
 
 
-function setUpJournals()
-{
+function setUpJournals() {
 	//TODO check if they have any journals in the first place
 	var journalsRef = database.ref("user/" + getCurrentUser().uid + "/Journals");
 	journals = journalsRef.once('value').then(function (snapshot){
@@ -32,11 +29,11 @@ function getJournals(data) {
 	var dataValue = data.val();
 	console.log(dataValue);
 	journals = dataValue;
-    displayJournals();
+	displayJournals();
 }
 
 function displayJournals() {
-    //TODO add listener for new journals
+	//TODO add listener for new journals
 	var journalValues = Object.values(journals);
 	journalsDisplay.innerHTML = "";
 	journalValues.forEach(function (journal) {
@@ -46,69 +43,63 @@ function displayJournals() {
 					<button class="btn btn-default" onclick="displayEntries(' + journal.DateCreated + ')">' +
 						journal.Title +
 					'</button>\
-                    </div>';
+				</div>';
 		}
 	});
 }
 
 function CreateNewJournal(){
-   document.getElementById("newJournal").style.display = "block";      
+	document.getElementById("newJournal").style.display = "block";
 }
-function submitCreateJournal()
-{
-   var newJournalTitle = document.getElementById("newJournalTitle").value;
-    if(newJournalTitle != null && newJournalTitle != "")
-    {
-      database.ref("user").child(getCurrentUser().uid).child("Journals").child(new Date().getTime()).set(
-	{
-		Title: newJournalTitle,
-		DateCreated: + new Date()
-	});
-    alert("New journal " + newJournalTitle + " has been created");
-    setUpJournals();
-    }
-    else
-    {
-        alert("Please type a valid title to create the new journal.");    
-    }
+
+function submitCreateJournal() {
+	var newJournalTitle = document.getElementById("newJournalTitle").value;
+	if(newJournalTitle != null && newJournalTitle != "") {
+		database.ref("user").child(getCurrentUser().uid).child("Journals").child(new Date().getTime()).set(
+		{
+			Title: newJournalTitle,
+			DateCreated: + new Date()
+		});
+		alert("New journal " + newJournalTitle + " has been created");
+		setUpJournals();
+		document.getElementById("newJournal").style.display = "none";
+	}
+	else {
+		alert("Please type a valid title to create the new journal.");
+	}
 }
 
 
 function displayEntries(journalDateCreated) {
-	
-   database.ref("user/" + getCurrentUser().uid + "/Journals/" + journalDateCreated + "/Entries").once('value').then(function (entries){
-     entriesDisplay.innerHTML = "";
-       if(entries.length ==0)
-           {
-               entriesDisplay.innerHTML = "This journal does not contain any entries";
-           }
-       else
-           {
-               	entries.forEach(function (entry) {
-		if (entry.val().length != 0) {
+	database.ref("user/" + getCurrentUser().uid + "/Journals/" + journalDateCreated + "/Entries").once('value').then(function (entries){
+	 entriesDisplay.innerHTML = "";
+		if(entries.length ==0) {
+				entriesDisplay.innerHTML = "This journal does not contain any entries";
+			}
+		else {
+			entries.forEach(function (entry) {
+				if (entry.val().length != 0) {
+					entriesDisplay.innerHTML +=
+						'<div class="container-fluid"><table align="left" class="table table-striped">\
+							<tbody>\
+								<tr>\
+									<td id="tableTitle">' + entry.val().Id + ':' + ' ' + entry.val().Title + '</td>\
+								</tr>\
+									<td>' + entry.val().Summary + '</td>\
+								<tr>\
+							</tbody>\
+						</table></div>';
+				}
+			});
 			entriesDisplay.innerHTML +=
-				'<div class="container-fluid"><table align="left" class="table table-striped">\
-				<tbody>\
-					<tr>\
-						<td id="tableTitle">' + entry.val().Id + ':' + ' ' + entry.val().Title + '</td>\
-					</tr>\
-						<td>' + entry.val().Summary + '</td>\
-					<tr>\
-				</tbody>\
-		</table></div>';
+				'<div id="journalBtn2" class="container-fluid"><button class="btn btn-info" onclick="goToJournal(' + journalDateCreated + ')">' +
+					"Go to journal" +
+				'</button></div>';
 		}
 	});
-	entriesDisplay.innerHTML +=
-		'<div id="journalBtn2" class="container-fluid"><button class="btn btn-info" onclick="goToJournal(' + journalDateCreated + ')">' +
-			"Go to journal" +
-		'</button></div>';
-           }
-   });
-   
-   
 }
 
 function goToJournal(journalDateCreated) {
-    localStorage.setItem("SelectedJournal", journalDateCreated)
+	localStorage.setItem("SelectedJournal", journalDateCreated)
 	window.location = "JournalEntryView.html";
 }
